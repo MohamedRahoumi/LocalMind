74e35595f1f73863aff62c6bd7434360e2071dbb
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2026-02-01 20:05:55 +0100

    Add LoginController.php

---
+++ brief12/src/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials, $request->filled('remember'))) {
+            $request->session()->regenerate();
+
+            return redirect()->intended(route('questions.index'))
+                ->with('success', 'Connexion réussie!');
+        }
+
+        return back()->withErrors([
+            'email' => 'Les identifiants fournis ne correspondent pas.',
+        ])->onlyInput('email');
+    }
+}
