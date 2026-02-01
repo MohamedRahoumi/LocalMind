5de2a9285612acbd9654f452d53c5c9caf015d5e
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2026-02-01 20:05:54 +0100

    Add DashboardController.php

---
+++ brief12/src/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use App\Models\Response;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Middleware\IsAdmin;
+
+class DashboardController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth', IsAdmin::class]);
+    }
+
+    public function index()
+    {
+        $stats = [
+            'total_users' => User::count(),
+            'total_questions' => Question::count(),
+            'total_responses' => Response::count(),
+            'recent_questions' => Question::with('user')->latest()->take(5)->get(),
+            'popular_questions' => Question::withCount('responses')
+                ->orderBy('responses_count', 'desc')
+                ->take(5)
+                ->get(),
+        ];
+
+        return view('dashboard', compact('stats'));
+    }
+}
